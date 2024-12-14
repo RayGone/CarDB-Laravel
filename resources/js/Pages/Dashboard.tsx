692d4d57150e6bc89468ea7a5cb3554cb8a47ad3
Regan <gregatom99@gmail.com>
2024-12-14 23:11:08 +1100

    UI: add interface types; demo use access-token to fetch cars from the api

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,8 +1,32 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
+import Table from '@/Components/Table';
+import { columnDef } from '@/model';
+import PrimaryButton from '@/Components/PrimaryButton';
+import { useEffect, useState } from 'react';
+import { CarResponse } from '@/types';
+
+
+const emptyCarResponse: CarResponse = {
+    cars: [],
+    total: 0
+}
 
 export default function Dashboard({ auth }: PageProps) {
+    const [cars, setCars] = useState(emptyCarResponse);
+    console.log({cars})
+    useEffect(() => {
+        window.axios.get("/api/cars", {
+            headers:{
+                Authorization: `Bearer ${auth.apiToken}`
+            }
+        }).then((data) => {
+            const response: CarResponse = data.data;
+            setCars(response);
+        });
+    },[]);
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -14,6 +38,9 @@ export default function Dashboard({ auth }: PageProps) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">You're logged in!</div>
+                        <button className="bg-blue-500 hover:bg-blue-700 active:bg-blue-600 text-white font-bold py-2 px-4 rounded"> Call API </button>
+                        <PrimaryButton>Call Another API</PrimaryButton>
+                        <Table columnDef={columnDef} data={cars} />
                     </div>
                 </div>
             </div>
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -8,5 +8,50 @@ export interface User {
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
+        apiToken: string;
     };
 };
+
+export interface Car {
+    id: number;
+    name: string;
+    origin: string;
+    model_year: number;
+    acceleration: number;
+    horsepower: number;
+    mpg: number;
+    weight: number;
+    cylinders: number;
+    displacement: number;
+  }
+
+  export interface CarResponse {
+    cars: Car[];
+    total: number;
+  }
+
+  export interface Column{
+    key: string;
+    header: string;
+    type: string;
+  }
+
+  export interface OP{
+    key: string;
+    value: string;
+  }
+
+  export interface FilterModel{
+    field: string;
+    ops: string;
+    value: string;
+  }
+
+  export interface DataFilterModel{
+    filter: FilterModel[];
+    limit: number;
+    order: string;
+    orderBy: string;
+    search: string;
+    page: number;
+  }
