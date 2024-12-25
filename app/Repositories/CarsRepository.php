94d15789898795f05c7cde2178b831ba8f508f4d
Regan <gregatom99@gmail.com>
2024-12-25 12:46:26 +1100

    API: apply all the search conditions from filter model to query data

--- app/Repositories/CarsRepository.php
+++ app/Repositories/CarsRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Models\Cars;
 use App\Dtos\FilterModel;
 use App\Enums\CarsAttributeEnum;
+use Illuminate\Support\Facades\Log;
 
 class CarsRepository{
     public function __construct(){}
@@ -13,17 +14,30 @@ class CarsRepository{
         $filterModel = FilterModel::init($filterModel);
 
         $query = Cars::orderBy($filterModel->orderBy->value, $filterModel->order->value);
+
         if(strlen($filterModel->search > 0)){
-            $query = $query->whereRaw('? ILIKE ?',[CarsAttributeEnum::NAME, "%{$filterModel->search}%"]);
+            $query = $query->where((CarsAttributeEnum::NAME)->value, 'LIKE',  "%{$filterModel->search}%")
+                            ->orWhere((CarsAttributeEnum::ORIGIN)->value, 'LIKE',  "%{$filterModel->search}%");
         }
 
-        $query = Cars::skip(($filterModel->page * $filterModel->limit))->take($filterModel->limit);
+
+        if(count($filterModel->filters) > 0){
+            $filter = array_shift($filterModel->filters);
+            $query = $query->where($filter->getField(), $filter->getOps(), $filter->getValue());
+
+            foreach($filterModel->filters as $filter){
+                $query = $query->where($filter->getField(), $filter->getOps(), $filter->getValue());
+            }
+        }
+        $total = $query->count();
+
+        $query = $query->skip(($filterModel->page * $filterModel->limit))->take($filterModel->limit);
         $cars = $query->get();
-        $total = Cars::count();
         return [
             "cars" => $cars,
             "total" => $total
         ];
+
         // $paginate = Cars::paginate($this->def_pageSize);
         // return [
         //     "cars" => $paginate->items(),
