ce9382552dbe688c6ece2fb993980d0d48c6dd10
Regan <gregatom99@gmail.com>
2025-01-14 15:07:30 +1100

    UI: fix filter button

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -48,11 +48,15 @@ function DashboardHeader({onSearch, onAdd=()=>{}, onOpenFilter=()=>{}}: {onSearc
                 </Dropdown>
             </div>
 
-            <ActionButton onClick={()=>onOpenFilter()} className='block lg:hidden' title={"Filters"}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 md:size-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-                </svg>
-            </ActionButton>
+            <div className='mr-2 block lg:hidden' onClick={()=>onOpenFilter()} >
+                <SecondaryButton className='hidden md:block lg:hidden'>Filters</SecondaryButton>
+
+                <ActionButton className='block md:hidden' title={"Filters"}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 md:size-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
+                    </svg>
+                </ActionButton>
+            </div>
 
         </div>
 
