d7d40cacee9a3692f5978f1bc7e528569cc03f5f
Regan <gregatom99@gmail.com>
2025-02-08 22:06:34 +1100

    Actions: CI/CD

---
+++ actions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Move Files Uploaded From Github Actions"
+sudo rm -rf ./composer.*
+sudo rm -rf ./vendor
+sudo rm -rf ./public/build
+
+sudo mv /home/runner/scp/vendor ./
+sudo mv /home/runner/scp/composer.json ./
+sudo mv /home/runner/scp/composer.lock ./
+sudo mv /home/runner/scp/build ./public/build
+
+sudo chown -R www-data:www-data storage bootstrap/cache
+sudo chmod -R 775 storage bootstrap/cache
+
+# Step 2: Clear Laravel cache
+echo "Clearing Laravel cache..."
+php artisan cache:clear
+
+# Step 3: Cache Laravel routes, config, and views
+echo "Caching Laravel routes, config, and views..."
+php artisan route:cache
+php artisan config:cache
+php artisan view:cache
+
+
